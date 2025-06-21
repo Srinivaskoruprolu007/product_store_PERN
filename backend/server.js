cc0104895f8526c84adfc5ab23d16e818556b52b
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-06-21 19:08:16 +0530

    feat: added basic setup

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+
+// basic setup
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
